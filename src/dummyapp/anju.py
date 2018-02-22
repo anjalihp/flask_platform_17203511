a1b06949d1be314e2fadcfd4cbe2bc5ed1557907
Anjali Gowda <anjali.pgowda@gmail.com>
2018-02-22 23:18:45 +0000

    second commit

--- src/dummyapp/anju.py
+++
@@ -1,6 +0,0 @@
-import systeminfo
-from systeminfo import main
-def main():
-    out= systeminfo.main()
-    print(out)
-    return
--- src/dummyapp/run.py
+++ src/dummyapp/run.py
@@ -3,4 +3,4 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.2', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
